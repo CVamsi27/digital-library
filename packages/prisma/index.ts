0c1811f1f2e35293879e2c2ef812a6765a1d28fa
Chandaluri Vamsi Krishna <cvamsik99@gmail.com>
2023-11-08 10:29:11 +0530

    fix: removed role export
    Signed-off-by: Chandaluri Vamsi Krishna <cvamsik99@gmail.com>

--- packages/prisma/index.ts
+++ packages/prisma/index.ts
@@ -15,5 +15,3 @@ const prisma = globalForPrisma.prisma ?? prismaClientSingleton();
 export default prisma;
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
-
-export { Role } from "@prisma/client";
