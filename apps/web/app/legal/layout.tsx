43674775fd3c389286205009bfb083090e84031d
Chandaluri Vamsi Krishna <cvamsik99@gmail.com>
2023-11-10 10:52:35 +0530

    feat: added legal docs for google auth
    Signed-off-by: Chandaluri Vamsi Krishna <cvamsik99@gmail.com>

---
+++ apps/web/app/legal/layout.tsx
@@ -0,0 +1,42 @@
+import "../globals.css";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import Providers from "@/app/providers";
+import Navbar from "@/app/_lib/components/navbar";
+import { getServerSession } from "next-auth";
+import Provider from "@/app/_trpc/Provider";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Legal Docs",
+  description: "Legal Docs",
+  icons: {
+    icon: "https://cdn.imgbin.com/23/15/24/imgbin-computer-icons-warranty-data-uri-scheme-font-warranty-qRwNmLRi3PNn5Acb5NrYi4gkg.jpg",
+  },
+};
+
+export default async function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const session = await getServerSession();
+  return (
+    <html lang="en">
+      <body className={inter.className} suppressHydrationWarning={true}>
+        <Providers session={session}>
+          <header>
+            <nav>
+              <Navbar />
+            </nav>
+          </header>
+          <main>
+            <Provider>{children}</Provider>
+          </main>
+          <footer></footer>
+        </Providers>
+      </body>
+    </html>
+  );
+}
